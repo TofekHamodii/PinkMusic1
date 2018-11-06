e2399f52185c9e5b63cb925f6515b821f331b29d
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-06 20:03:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -469,6 +469,30 @@ function play(guild, song) {
       }
     }).then(message =>{message.delete(5000)})
 }
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login('NTA5Mzc5NTE5MDIzNDE1Mjk2.DsNo_Q.BKGSS2DICkQ5c9zIevIrPWKxlQw');
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '1join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
  
 client.on('message', message => {
   if (!message.content.startsWith(PREFIX)) return;
